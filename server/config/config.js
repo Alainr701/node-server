001d00f9de0bc11d29dd7e2e2ddcf71398148c11
Alainr701 <a701e3127@gmail.com>
2021-12-14 12:28:27 -0400

    Second App

--- server/config/config.js
+++ server/config/config.js
@@ -4,11 +4,11 @@ process.env.PORT = process.env.PORT || 3000;
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
-//bd
-// let urlDB;
-// if (process.env.NODE_ENV === 'dev') {
-//     urlDB = 'mongodb://localhost:27017/cafe';
-// } else {
+bd
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
     urlDB = 'mongodb+srv://alainr:3127@cluster0.zt5yv.mongodb.net/cafe';
-// }
+}
 process.env.URLDB = urlDB;
